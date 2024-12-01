0c35f541105deca5387a335ea7a3d223705c3ea6
unknown <tvladislav0504@gmail.com>
2024-12-01 12:28:34 +0200

    211 add swagger ui

--- starter/app.js
+++ starter/app.js
@@ -7,6 +7,11 @@ const cors = require('cors')
 const xss = require('xss-clean')
 const rateLimiter = require('express-rate-limit')
 
+// swagger
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
 const express = require('express')
 const app = express()
 
@@ -23,17 +28,22 @@ const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
 // extra packages
-app.set('trust proxy', 1)
+
+app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 app.use(
   rateLimiter({
     windowsMs: 15 * 60 * 1000, // 15 minutes
     max: 100, // limit each IP to 100 requests per windowsMs
   })
 )
-app.use(express.json())
-app.use(helmet())
-app.use(cors())
-app.use(xss())
+
+app.get('/', (req, res) => {
+  res.send('<h1>jobs API</h1><a href ="/api-docs">Documentation</a>')
+})
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 // routes
 app.use('/api/v1/auth', authRouter)
