b45bd9e82387d5adf3e74f1d4b6aa3a96b840bb9
unknown <tvladislav0504@gmail.com>
2024-12-01 10:46:34 +0200

    194 remove job

--- starter/controllers/jobs.js
+++ starter/controllers/jobs.js
@@ -47,7 +47,19 @@ const updateJob = async (req, res) => {
   res.status(StatusCodes.OK).json({ job })
 }
 const deleteJob = async (req, res) => {
-  res.send('delete job')
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req
+
+  const job = await Job.findByIdAndRemove({
+    _id: jobId,
+    createdBy: userId,
+  })
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`)
+  }
+  res.status(StatusCodes.OK).send()
 }
 
 module.exports = {
